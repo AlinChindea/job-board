a05a143fc06d952fdf58806224661f16728cc8a7
Alin Chindea <alin.chindea@gmail.com>
2024-05-25 20:06:41 +0200

    Update the standard job-card and welcome page
    - welcome page now includes a job search section at the top

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,16 +1,16 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+<x-card class="flex flex-col text-center">
     <div class="self-start text-sm">Laracasts</div>
-    <div class="py-8 font-bold">
-        <h3>Video Producer</h3>
-        <p>Full Time - From $60,000</p>
+    <div class="py-8">
+        <h3 class="group-hover:text-blue-600 transition-colors duration-600 text-xl font-bold">Video Producer</h3>
+        <p class="text-sm mt-4">Full Time - From $60,000</p>
     </div>
     <div class="flex justify-between items-center mt-auto">
         <div>
-            <x-tag>Remote</x-tag>
-            <x-tag>Full Time</x-tag>
-            <x-tag>Norsk</x-tag>
+            <x-tag size="small">Remote</x-tag>
+            <x-tag size="small">Full Time</x-tag>
+            <x-tag size="small">Norsk</x-tag>
         </div>
 
-        <img src="http://placehold.it/42/42" alt="" class="rounded-xl">
+        <x-employer-logo :width='42'/>
     </div>
-</div>
+</x-card>
