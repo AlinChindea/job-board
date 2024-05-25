7990e38914a78d730e5f66a45fd9362fb2972e36
Alin Chindea <alin.chindea@gmail.com>
2024-05-25 20:05:28 +0200

    Create card component
    - include a logo component and a job-card-wide component

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,18 @@
+<x-card class="flex gap-x-6">
+    <div>
+        <x-employer-logo />
+    </div>
+
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 transition-colors duration-300">Video Producer</h3>
+        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
+    </div>
+
+    <div>
+        <x-tag>Remote</x-tag>
+        <x-tag>Full Time</x-tag>
+        <x-tag>Norsk</x-tag>
+    </div>
+</x-card>
