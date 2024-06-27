ea48b9f16ea1cb03eb8fd7e483d2bfe8f560d78c
Alin Chindea <alin.chindea@gmail.com>
2024-06-27 20:09:02 +0200

    Add some seeds and replace hard-coded values

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,18 +1,20 @@
+@props(['job'])
+
 <x-card class="flex gap-x-6">
     <div>
         <x-employer-logo />
     </div>
 
     <div class="flex-1 flex flex-col">
-        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+        <a href="#" class="self-start text-sm text-gray-400">{{ $job->employer->name }}</a>
 
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 transition-colors duration-300">Video Producer</h3>
-        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 transition-colors duration-300">{{ $job->title }}</h3>
+        <p class="text-sm text-gray-400 mt-auto">{{ $job->schedule }} - From {{ $job->salary }}</p>
     </div>
 
     <div>
-        <x-tag>Remote</x-tag>
-        <x-tag>Full Time</x-tag>
-        <x-tag>Norsk</x-tag>
+        @foreach ($job->tags as $tag)
+            <x-tag :$tag size="small" />
+        @endforeach
     </div>
 </x-card>
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,14 +1,16 @@
+@props(['job'])
+
 <x-card class="flex flex-col text-center">
-    <div class="self-start text-sm">Laracasts</div>
+    <div class="self-start text-sm">{{ $job->employer->name }}</div>
     <div class="py-8">
-        <h3 class="group-hover:text-blue-600 transition-colors duration-600 text-xl font-bold">Video Producer</h3>
-        <p class="text-sm mt-4">Full Time - From $60,000</p>
+        <h3 class="group-hover:text-blue-600 transition-colors duration-600 text-xl font-bold">{{ $job->title }}</h3>
+        <p class="text-sm mt-4">{{ $job->schedule }} - From {{ $job->salary }}</p>
     </div>
     <div class="flex justify-between items-center mt-auto">
         <div>
-            <x-tag size="small">Remote</x-tag>
-            <x-tag size="small">Full Time</x-tag>
-            <x-tag size="small">Norsk</x-tag>
+            @foreach ($job->tags as $tag)
+                <x-tag :$tag size="small" />
+            @endforeach
         </div>
 
         <x-employer-logo :width='42'/>
