dd9d18c32949b040e19c1191c75f7c2948e79126
Alin Chindea <alin.chindea@gmail.com>
2024-07-17 14:50:39 +0200

    Update routes with relevant middleware

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,18 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class, 'index']);
 
-Route::get('/register', [RegisteredUserController::class, 'create']);
-Route::post('/register', [RegisteredUserController::class, 'store']);
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [RegisteredUserController::class, 'create']);
+    Route::post('/register', [RegisteredUserController::class, 'store']);
+
+    Route::get('/login', [LoginController::class, 'create']);
+    Route::post('/login', [LoginController::class, 'store']);
+});
+
+Route::delete('/logout', [LoginController::class, 'destroy'])->middleware('auth');
